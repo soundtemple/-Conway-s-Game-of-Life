c0931a96a949f26d5bfc7393be67ac16b2ecb733
Tim Walter <twwalter@gmail.com>
2017-03-15 11:37:44 +1100

    refactor variable names for readability

--- game_of_life.js
+++ game_of_life.js
@@ -7,25 +7,25 @@ var genWeighting = 0.05; //sets start weighting for generation 0=Off 1=On
 
 
 // create grid rows
-_.range(rows).map(function (elem, index) {
-  var rowId = 'row' + index; //create id for the div
+_.range(rows).map(function (rowNum) {
+  var rowId = 'row' + rowNum; //create id for the div
   var $newDiv = $("<div>").attr('id', rowId).addClass("row"); //create and empty div with iD
   var $divLoc = $('.board'); //locate div container;
   $divLoc.append($newDiv);
-  createCellsInRow(index);
+  createCellsInRow(rowNum);
 });
 
 // create grid cells in rows (columns)
-function createCellsInRow(rowId) {
-  _.range(columns).map(function (elem, index) {
-    var cellId = 'cell' + ((rowId * columns) + index); //create id for the div
+function createCellsInRow(rowNum) {
+  _.range(columns).map(function (cellNum) {
+    var cellId = 'cell' + ((rowNum * columns) + cellNum); //create id for the div
     var $newDiv = $("<div>").attr('id', cellId).addClass("cell"); //create and empty div with iD
-    var $divLoc = $("#row" + rowId); //locate div container;
+    var $divLoc = $("#row" + rowNum); //locate div container;
     $divLoc.append($newDiv);
   });
 };
 
-// initialise board
+// initialise board. create array and with values 0 or 1 for on/off
 function initBoard(columns, rows) {
   var cellNum = 0;
   while (cellNum < (columns * rows)) {
@@ -67,22 +67,22 @@ function newGeneration() {
 }
 
 // Checking each neighbour state. Tabulate a score
-function checkNeighbours(cellIndex) {
+function checkNeighbours(currentCell) {
   var cellScore;
   var cellTotal = 0;
-  neighbours.forEach(function (element) {
+  neighbours.forEach(function (neighbour) {
     // Account for neighbours outside the array range
-    isNaN(board[cellIndex + element]) ? cellScore = 0 : cellScore = board[cellIndex + element];
+    isNaN(board[currentCell + neighbour]) ? cellScore = 0 : cellScore = board[currentCell + neighbour];
     cellTotal += cellScore;
   });
   return cellTotal;
 }
 
 // Cell dies unless neighbour score is 2-3 OR dead cell has neighbour score ==3
-function newCellState(cellIndex, cellScore) {
+function newCellState(currentCell, cellScore) {
   var newCellState = 0;
-  cellScore == 2 || cellScore == 3 && board[cellIndex] ? newCellState = 1 : newCellState = 0;
-  if (cellScore == 3 && board[cellIndex] == 0) newCellState = 1;
+  cellScore == 2 || cellScore == 3 && board[currentCell] ? newCellState = 1 : newCellState = 0;
+  if (cellScore == 3 && board[currentCell] == 0) newCellState = 1;
   return newCellState;
 }
 
